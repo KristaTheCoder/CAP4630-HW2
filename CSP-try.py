45abef78d2648024987ab7a77e31fbbe120f74d6
Krista Shuckerow <kshuckerow@gmail.com>
2017-10-17 15:48:01 -0400

    added beginning of hint 5

--- CSP-try.py
+++ CSP-try.py
@@ -25,10 +25,27 @@ def hint2(student, color):
     return student, color
 
 '''
+FIXME
 Omar's design went somewhat farther than the silver airplane.
 '''
 def hint3(student, color, distance):
-    if(student == "Omar")
+    if(student == "Omar"):
+        return
+    return student, color, distance
+
+'''
+FIXME
+The pink plane went 10 feet further than the black plane
+TODO: make sure that the final anwers are within 10 of eachother
+'''
+#try preprocessing need to figure out how to check all other in set
+def hint5(color, distance):
+    if(color == "pink" and distance == 15):
+        return
+    if(color == "black" and distance == 45):
+        return
+    return color, distance
+
 
 problem.addConstraint(FunctionConstraint(hint1), ["students", "distances"])
 #Check that hint 1 constraints are working
@@ -36,4 +53,9 @@ print len(problem.getSolutions())
 
 #check that hint2 constraints are working
 problem.addConstraint(FunctionConstraint(hint2), ["students", "colors"])
+print len(problem.getSolutions())
+
+
+#check that hint5 constraint is working.
+problem.addConstraint(FunctionConstraint(hint5), ["colors", "distances"])
 print len(problem.getSolutions())
