68fa5254a25beafbbc2b8eef5f6386d985153be4
Krista Shuckerow <kshuckerow@gmail.com>
2017-10-18 18:28:53 -0400

    Narrowed to 3

--- CSP-try.py
+++ CSP-try.py
@@ -1,9 +1,5 @@
 from constraint import *
 
-#set-up problem
-
-
-
 '''
 Henrietta's design went 35 feet
 '''
@@ -70,67 +66,62 @@ def hint5(student, color, distance):
         return False
     return True
 
-#filter black based on ella
-def hint5a(color, distance):
-    global ellaD
-    if(color == "black"):
-        if(distance + 10  in ellaD):
-            return True
-        else:
-             return False
-    return True
 
 
 def main():
 
-    print "main"
     problem = Problem()
-    students1 = ["Ella", "Henrietta", "Omar", "Valerie"]
-    colors1 = ["black", "blue", "pink", "silver"]
-    distances1 = [15, 25, 35, 45]
-    problem.addVariable("students", students1)
-    problem.addVariable("colors", colors1)
-    problem.addVariable("distances", distances1)
+    students = ["Ella", "Henrietta", "Omar", "Valerie"]
+    colors = ["black", "blue", "pink", "silver"]
+    distances = [15, 25, 35, 45]
 
-    problem.addConstraint(FunctionConstraint(hint1), ["students", "distances"])
-    problem.addConstraint(FunctionConstraint(hint2), ["students", "colors"])
-    problem.addConstraint(FunctionConstraint(hint3a), ["students", "colors", "distances"])
-    problem.addConstraint(FunctionConstraint(hint4), ["students", "colors", "distances"])
-    problem.addConstraint(FunctionConstraint(hint5), ["students", "colors", "distances"])
-
-    #pink is 10 more than black
-    for pink in problem.getSolutions():
-        if(pink["colors"] == "pink"):
-            possiblePink = False
-            for black in problem.getSolutions():
-                if(black["colors"] == "black"):
-                    if(pink["distances"] - black["distances"] == 10):
-                        possiblePink == True
-            if( not possiblePink):
-                problem.addConstraint(lambda a, b: not(a != pink["distances"] and b == pink["colors"]), ("distances", "colors"))
-
-    print len(problem.getSolutions())
-    for answer in problem.getSolutions():
-        print answer.items()
-    #black is 10 less than pink
-
-    for black1 in problem.getSolutions():
-        if(black1["colors"] == "black"):
-            possibleBlack = False
-            for pink1 in problem.getSolutions():
-                if(pink1["colors"] == "pink"):
-                    if(pink1["distances"] - black1["distances"] == 10):
-                        possibleBlack == True
-            if(not possibleBlack):
-                problem.addConstraint(lambda a, b: not(a == black1["distances"] and b == black1["colors"]), ("distances", "colors"))
+    for i in range(len(students)):
+        problem.addVariable("students" + str(i), students)
+        problem.addVariable("colors" + str(i), colors)
+        problem.addVariable("distances" + str(i), distances)
 
     problem.addConstraint(AllDifferentConstraint())
+    problem.addConstraint(lambda a, b, c, d: a == "Ella" and b == "Henrietta" and c == "Omar" and d == "Valerie", ("students0", "students1", "students2", "students3"))
+    # problem.addConstraint(lambda d: d == "Valerie", ("students3"))
+    for i in range(len(students)):
+        problem.addConstraint(FunctionConstraint(hint1), ["students" + str(i), "distances" + str(i)])
+        problem.addConstraint(FunctionConstraint(hint2), ["students" + str(i), "colors" + str(i)])
+        problem.addConstraint(FunctionConstraint(hint3a), ["students" + str(i), "colors" + str(i), "distances" + str(i)])
+        problem.addConstraint(FunctionConstraint(hint4), ["students" + str(i), "colors" + str(i), "distances" + str(i)])
+        problem.addConstraint(FunctionConstraint(hint5), ["students" + str(i), "colors" + str(i), "distances" + str(i)])
+
+    #
+    # #pink is 10 more than black
+    # for pink in problem.getSolutions():
+    #     if(pink["colors"] == "pink"):
+    #         possiblePink = False
+    #         for black in problem.getSolutions():
+    #             if(black["colors"] == "black"):
+    #                 if(pink["distances"] - black["distances"] == 10):
+    #                     possiblePink == True
+    #         if( not possiblePink):
+    #             problem.addConstraint(lambda a, b: not(a != pink["distances"] and b == pink["colors"]), ("distances", "colors"))
+    #
+    # print len(problem.getSolutions())
+    # for answer in problem.getSolutions():
+    #     print answer.items()
+    # #black is 10 less than pink
+    #
+    # for black1 in problem.getSolutions():
+    #     if(black1["colors"] == "black"):
+    #         possibleBlack = False
+    #         for pink1 in problem.getSolutions():
+    #             if(pink1["colors"] == "pink"):
+    #                 if(pink1["distances"] - black1["distances"] == 10):
+    #                     possibleBlack == True
+    #         if(not possibleBlack):
+    #             problem.addConstraint(lambda a, b: not(a == black1["distances"] and b == black1["colors"]), ("distances", "colors"))
+    #
 
     print len(problem.getSolutions())
     for answer in problem.getSolutions():
-        print answer
-
-    #need all different between possible sets
+        for key, value in sorted(answer.iteritems(), key=lambda (k,v): (v,k)):
+            print "%s: %s" % (value, key)
 
 
 
